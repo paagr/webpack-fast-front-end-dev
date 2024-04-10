7e1ba71c67ce347d5a8aa1a44212b09a3dc2e4c1
Paolo Mason <mason.paolo@gmail.com>
2024-04-10 12:30:51 +0200

    add preload image

--- src/index.js
+++ src/index.js
@@ -1,191 +1,3 @@
-// import './styles.scss';
-
-// import gsap from 'gsap';
-
-// const isTouchDevice = 'ontouchstart' in window;
-
-// const imgW = 520;
-// const imgH = 520;
-
-// const createImagePop = () => {
-// 	let lastX = -Infinity;
-// 	let lastY = -Infinity;
-
-// 	const imgContainer = document.querySelector('.img-container');
-// 	const images = imgContainer.querySelectorAll('.img-cursor');
-
-// 	let ongoingX = null;
-// 	let ongoingTime = null;
-// 	let velocity = 0;
-
-// 	window.addEventListener('mousemove', (e) => {
-// 		const { x, y } = e;
-
-// 		const currentTime = Date.now();
-// 		const currentX = e.pageX;
-
-// 		if (ongoingX !== null && ongoingTime !== null) {
-// 			const deltaTime = currentTime - ongoingTime;
-// 			const deltaX = currentX - ongoingX;
-// 			velocity = deltaX / deltaTime;
-// 		}
-
-// 		ongoingX = currentX;
-// 		ongoingTime = currentTime;
-
-// 		const distanceX = Math.abs(x - lastX);
-// 		const distanceY = Math.abs(y - lastY);
-// 		if (distanceX >= 55 || distanceY >= 55) {
-// 			lastX = x;
-// 			lastY = y;
-
-// 			const randomIndex = Math.floor(Math.random() * images.length);
-// 			const randomImage = images[randomIndex];
-
-// 			if (randomImage) {
-// 				const image = randomImage.cloneNode(); // Create a clone of the randomly selected image
-// 				image.style.position = 'absolute';
-// 				image.style.width = `${imgW}px`;
-// 				image.style.height = `${imgH}px`;
-// 				image.style.objectFit = 'contain';
-// 				image.style.left = `${x - imgW / 2}px`;
-// 				image.style.top = `${y - imgH / 2}px`;
-
-// 				document.body.appendChild(image);
-
-// 				gsap.fromTo(
-// 					image,
-// 					{ scale: 0, rotation: 0 },
-// 					{
-// 						duration: 0.89,
-// 						scale: 1,
-// 						x: `+=${velocity * 55}`,
-// 						y: `+=${velocity * 55}`,
-// 						rotation: velocity * 2.33,
-// 						ease: 'power1.inOut',
-// 					}
-// 				);
-
-// 				setTimeout(() => {
-// 					gsap.to(image, {
-// 						duration: 1,
-// 						y: '+=100%',
-// 						opacity: 0,
-// 						onComplete: () => {
-// 							document.body.removeChild(image); // Remove the image after the animation completes
-// 						},
-// 					});
-// 				}, 3000);
-// 			}
-// 		}
-// 	});
-// };
-
-// if (!isTouchDevice) {
-// 	createImagePop();
-// }
-
-// import './styles.scss';
-// import gsap from 'gsap';
-
-// const isTouchDevice = 'ontouchstart' in window;
-
-// const imgW = 520;
-// const imgH = 520;
-
-// const getUnsplashImage = () => {
-// 	return new Promise((resolve, reject) => {
-// 		const imageUrl = 'https://source.unsplash.com/random';
-// 		const image = new Image();
-// 		image.onload = () => {
-// 			resolve(image.src);
-// 		};
-// 		image.onerror = (error) => {
-// 			reject(error);
-// 		};
-// 		image.src = imageUrl;
-// 	});
-// };
-
-// const createImagePop = () => {
-// 	let lastX = -Infinity;
-// 	let lastY = -Infinity;
-
-// 	const imgContainer = document.querySelector('.img-container');
-
-// 	let ongoingX = null;
-// 	let ongoingTime = null;
-// 	let velocity = 0;
-
-// 	window.addEventListener('mousemove', async (e) => {
-// 		const { x, y } = e;
-
-// 		const currentTime = Date.now();
-// 		const currentX = e.pageX;
-
-// 		if (ongoingX !== null && ongoingTime !== null) {
-// 			const deltaTime = currentTime - ongoingTime;
-// 			const deltaX = currentX - ongoingX;
-// 			velocity = deltaX / deltaTime;
-// 		}
-
-// 		ongoingX = currentX;
-// 		ongoingTime = currentTime;
-
-// 		const distanceX = Math.abs(x - lastX);
-// 		const distanceY = Math.abs(y - lastY);
-// 		if (distanceX >= 55 || distanceY >= 55) {
-// 			lastX = x;
-// 			lastY = y;
-
-// 			const imageUrl = await getUnsplashImage(); // Fetch a new image URL from Unsplash
-// 			const image = new Image();
-// 			image.onload = () => {
-// 				image.style.position = 'absolute';
-// 				image.style.width = `${imgW}px`;
-// 				image.style.height = `${imgH}px`;
-// 				image.style.objectFit = 'contain';
-// 				image.style.left = `${x - imgW / 2}px`;
-// 				image.style.top = `${y - imgH / 2}px`;
-
-// 				document.body.appendChild(image);
-
-// 				gsap.fromTo(
-// 					image,
-// 					{ scale: 0, rotation: 0 },
-// 					{
-// 						duration: 0.89,
-// 						scale: 1,
-// 						x: `+=${velocity * 55}`,
-// 						y: `+=${velocity * 55}`,
-// 						rotation: velocity * 2.33,
-// 						ease: 'power1.inOut',
-// 					}
-// 				);
-
-// 				setTimeout(() => {
-// 					gsap.to(image, {
-// 						duration: 1,
-// 						y: '+=100%',
-// 						opacity: 0,
-// 						onComplete: () => {
-// 							document.body.removeChild(image); // Remove the image after the animation completes
-// 						},
-// 					});
-// 				}, 3000);
-// 			};
-// 			image.onerror = (error) => {
-// 				console.error('Error loading image:', error);
-// 			};
-// 			image.src = imageUrl;
-// 		}
-// 	});
-// };
-
-// if (!isTouchDevice) {
-// 	createImagePop();
-// }
-
 import './styles.scss';
 import gsap from 'gsap';
 
@@ -194,46 +6,53 @@ const isTouchDevice = 'ontouchstart' in window;
 const imgW = 520;
 const imgH = 520;
 
-// const getUnsplashImage = () => {
-// 	return new Promise((resolve, reject) => {
-// 		const imageUrl = 'https://source.unsplash.com/random';
-// 		const image = new Image();
-// 		image.onload = () => {
-// 			resolve(image.src);
-// 		};
-// 		image.onerror = (error) => {
-// 			reject(error);
-// 		};
-// 		image.src = imageUrl;
-// 	});
-// };
-
-const searchTerms = ['night', 'developer', 'design', 'code', 'javascript', 'typography', 'party', 'dj', 'singer'];
+const searchTerms = [
+	'night',
+	'model',
+	'girl',
+	'female',
+	'developer',
+	'design',
+	'code',
+	'javascript',
+	'typography',
+	'party',
+	'dj',
+];
 
 const getRandomSearchTerm = () => {
 	return searchTerms[Math.floor(Math.random() * searchTerms.length)];
 };
 
-const getUnsplashImage = () => {
+const preloadImage = (url) => {
 	return new Promise((resolve, reject) => {
-		const searchTerm = getRandomSearchTerm();
-		const imageUrl = `https://source.unsplash.com/random/?${searchTerm}`;
 		const image = new Image();
 		image.onload = () => {
-			resolve(image.src);
+			resolve(url);
 		};
 		image.onerror = (error) => {
 			reject(error);
 		};
-		image.src = imageUrl;
+		image.src = url;
+	});
+};
+
+const getUnsplashImage = () => {
+	return new Promise(async (resolve, reject) => {
+		const searchTerm = getRandomSearchTerm();
+		const imageUrl = `https://source.unsplash.com/random/?${searchTerm}`;
+		try {
+			await preloadImage(imageUrl); // Preload the image
+			resolve(imageUrl);
+		} catch (error) {
+			reject(error);
+		}
 	});
 };
 
 const createImagePop = async () => {
 	let lastX = -Infinity;
 	let lastY = -Infinity;
-
-	const imgContainer = document.querySelector('.img-container');
 
 	let ongoingX = null;
 	let ongoingTime = null;
@@ -260,7 +79,7 @@ const createImagePop = async () => {
 			lastX = x;
 			lastY = y;
 
-			const imageUrl = await getUnsplashImage(); // Fetch a new image URL from Unsplash
+			const imageUrl = await getUnsplashImage();
 			const image = new Image();
 			image.onload = () => {
 				image.style.position = 'absolute';
@@ -278,8 +97,8 @@ const createImagePop = async () => {
 					{
 						duration: 0.89,
 						scale: 1,
-						x: `+=${velocity * 55}`,
-						y: `+=${velocity * 55}`,
+						x: `+=${velocity * 21}`,
+						y: `+=${velocity * 21}`,
 						rotation: velocity * 2.33,
 						ease: 'power1.inOut',
 					}
@@ -291,7 +110,7 @@ const createImagePop = async () => {
 						y: '+=100%',
 						opacity: 0,
 						onComplete: () => {
-							document.body.removeChild(image); // Remove the image after the animation completes
+							document.body.removeChild(image);
 						},
 					});
 				}, 3000);
